ed2f5bcaf896e93aa9e4f41f5b43beff2e842965
sandybhatta <105071143+sandybhatta@users.noreply.github.com>
2024-07-02 12:56:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function getFormvalue() {
     //Write your code here
-
+	const inps=document.querySelectorAll("input");
+	
+	
+	
+	let form=document.querySelector("form");
+	form.addEventListener('submit', (e)=>{
+			e.preventDefault();
+		let innerT1="";
+		inps.forEach((val)=>{
+			if(val.type=="text"){
+			innerT1=innerT1+" " +val.value;
+			}
+		})
+				alert(innerT1.trim())
+			
+		})
 }
+getFormvalue()
